058f1986fd786735ced31c67075956186be32e19
Mohan Wadia <96598878+mohanwadia@users.noreply.github.com>
2026-06-25 12:07:12 +1000

    Update website URL in config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://mohanwadia.com", // replace this with your deployed domain
+  website: "https://mohanwadia.com/blog", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
